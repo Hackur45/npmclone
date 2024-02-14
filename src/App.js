99937e0c59a5c109b688ed3a0f6a7abb72f60455
Mandar <mandargurjar51@gmail.com>
2024-02-14 13:02:05 +0530

    14-02-2024 commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+
+import NavScrollExample from "./components/Navbar";
+import Navbarup from "./components/NavbarUp";
+import MainPagecontent from "./components/MainPagecontent";
 
 function App() {
   return (
     <>
-    <Navbar/>
+      <div className="nav-ext">
+        <div className="nav-up">
+          <Navbarup />
+        </div>
+        <div>
+          <NavScrollExample style={{ marginTop: "20px" }} />
+        </div>
+        <MainPagecontent/>
+      </div>
     </>
   );
 }
+
+// class App extends Component {
+//   render() {
+//       const myStyle = {
+//           backgroundImage: "url(/npmbg.jpeg)",
+//           height: "100vh",
+//           marginTop: "-70px",
+//           fontSize: "50px",
+//           backgroundSize: "cover",
+//           backgroundRepeat: "no-repeat",
+//       };
+//       return (
+//           <div style={myStyle}>
+//               <h1>GeeksForGeeks</h1>
+//           </div>
+//       );
+//   }
+// }
 
 export default App;
