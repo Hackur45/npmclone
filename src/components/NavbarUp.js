f41f633acd2c09c7c7cea9fdf730f31ef879fbf9
Mandar <mandargurjar51@gmail.com>
2024-02-14 19:01:40 +0530

    worked on navbarup

--- src/components/NavbarUp.js
+++ src/components/NavbarUp.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
+import BlackHeartIcon from "./BlackHeartIcon";
+import "./NavBarup.css";
 
 export default function Navbarup() {
-    const mystyle={
-        backgroundColor: 'white',
-        height: '60px', // Adjust height as needed
-        width: '100%', 
-        borderBottom: '2px solid #ccc'// Adjust width as needed
-    };
+  const mystyle = {
+    display: "flex",
+    alignItems: "flex-start",
+    alignContent: "center",
+    flexWrap: "wrap",
+    backgroundColor: "white",
+    height: "60px", // Adjust height as needed
+    width: "100%",
+    borderBottom: "2px solid #ccc", // Adjust width as needed
+    gap : "15px",
+    padding : "0px 0px 0px 2px" 
+  };
 
-    return (
-        <div style={mystyle}>
-           
-        </div>
-    );
+  return (
+    <div style={mystyle}>
+      <div className="heart">
+        <BlackHeartIcon />
+      </div>
+      <div className="sidetext">
+        <p>Numerous Philantrophic Misanthropes</p>
+      </div>
+    </div>
+  );
 }
