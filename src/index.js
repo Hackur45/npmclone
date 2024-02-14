f9a8b314ed6b2523f7eed3ee36e0ebe725ed79ad
Mandar <mandargurjar51@gmail.com>
2024-02-14 12:51:56 +0530

    In this I am adding bg-image to all

--- src/index.js
+++ src/index.js
@@ -14,4 +14,8 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+document.body.style.backgroundImage = 'url("https://static-production.npmjs.com/abf53a31b2da4657a1a004ee9358551c.png")'; // Replace with the URL of your image
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundPosition = 'center';
+document.body.style.minHeight = '100vh';
 reportWebVitals();
